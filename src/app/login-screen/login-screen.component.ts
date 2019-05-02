746e5910a951636e52da2d99b83a3999b6eb720a
Stanisław Góra <stasgora@gmail.com>
2019-05-02 07:59:44 +0200

    strong password validation

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -18,7 +18,7 @@ export class LoginScreenComponent implements OnInit {
 			']+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*' +
 			'[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[' +
 			'01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')]],
-		"password": ['', Validators.required]
+		"password": ['', [Validators.required, Validators.pattern('(?=(.*[a-z]){2})(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*].{7,}')]]
 	});
 	matcher = new EmailErrorStateMatcher();
 
