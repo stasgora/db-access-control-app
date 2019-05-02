196a21304da519ee5c9cedf900d4bf7a4e12dc6a
Stanisław Góra <stasgora@gmail.com>
2019-05-02 20:48:30 +0200

    pass to short error message

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -10,15 +10,14 @@ import { ErrorStateMatcher } from "@angular/material";
 })
 export class LoginScreenComponent implements OnInit {
 	private passwordHidden: boolean = true;
-	private password: string;
-	private email: string;
 
 	formControl = this.formBuilder.group({
 		"email": ['', [Validators.required, Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-' +
 			']+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*' +
 			'[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[' +
 			'01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')]],
-		"password": ['', [Validators.required, Validators.pattern('(?=(.*[a-z]){2})(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*].{7,}')]]
+		"password": ['', [Validators.required, Validators.minLength(8),
+			Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*].{3,}')]]
 	});
 	matcher = new EmailErrorStateMatcher();
 
